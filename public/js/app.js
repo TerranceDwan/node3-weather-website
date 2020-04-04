034e7869e91412f178700e090d5c6186f22883eb
TerranceDwan <terrance4869@gmail.com>
2020-04-04 11:09:40 -0500

    Removed Console Log

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,9 @@
-console.log('Client side javascript is loaded')
-
 const weatherForm = document.querySelector('form')
 const searchEl = document.querySelector('input')
 const messageOne = document.querySelector('#message-one')
 const messageTwo = document.querySelector('#message-two')
 
-weatherForm.addEventListener('submit', e => {
+weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
   const location = searchEl.value
@@ -18,10 +16,10 @@ weatherForm.addEventListener('submit', e => {
   fetch('http://localhost:3000/weather?address=' + location, {
     headers: {
       'Content-Type': 'application/json',
-      Accept: 'application/json'
-    }
-  }).then(response => {
-    response.json().then(data => {
+      Accept: 'application/json',
+    },
+  }).then((response) => {
+    response.json().then((data) => {
       if (data.error) {
         messageOne.textContent = data.error
       } else {
